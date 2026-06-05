f5b2af249f119c3712c507cacb51cc04d159d0f4
Johnny <johnny.labes@ufpr.br>
2026-06-05 15:05:07 -0300

    feat: js file

---
+++ src/js/curriculo.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const yearElement = document.getElementById("current-year");
+
+    if (yearElement) {
+        yearElement.textContent = new Date().getFullYear();
+    }
+});
